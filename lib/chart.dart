29410b16ebfc866ec35d0bc78bb8102d919bb15a
Amany Gharib <amanygh12@gmail.com>
2020-10-10 00:07:33 -0700

    adding chart bar

--- lib/chart.dart
+++ lib/chart.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-
+import 'widgets/chart_bar.dart';
 import 'models/transaction.dart';
 
 class Chart extends StatelessWidget {
-
   final List<Transaction> recentTransactions;
 
-  const Chart( this.recentTransactions);
+  const Chart(this.recentTransactions);
   List<Map<String, Object>> get groubedTransactionValues {
     return List.generate(7, (index) {
       final weekDay = DateTime.now().subtract(Duration(days: index));
@@ -17,13 +16,21 @@ class Chart extends StatelessWidget {
         if (recentTransactions[i].date.day == weekDay.day &&
             recentTransactions[i].date.month == weekDay.month &&
             recentTransactions[i].date.year == weekDay.year) {
-            totalSum += recentTransactions[i].price;
+          totalSum += recentTransactions[i].price;
         }
       }
       print(DateFormat.E().format(weekDay));
       print(totalSum);
-      return {'Day': DateFormat.E().format(weekDay),
-       'price': totalSum};
+      return {
+        'Day': DateFormat.E().format(weekDay).substring(0, 1),
+        'price': totalSum,
+      };
+    });
+  }
+
+  double get maxSpendding {
+    return groubedTransactionValues.fold(0.0, (sum, item) {
+      return sum + item['price'];
     });
   }
 
@@ -33,8 +40,22 @@ class Chart extends StatelessWidget {
     return Card(
       elevation: 5,
       margin: EdgeInsets.all(20),
-      child: Row(
-        children: [],
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groubedTransactionValues.map((data) {
+            return Flexible(
+              fit: FlexFit.tight,
+                        child: ChartBar(
+                  data['Day'],
+                  maxSpendding == 0.0
+                      ? 0.0
+                      : (data['price'] as double) / maxSpendding,
+                  data['price']),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
